b154bd0928747ce25e57c8a230649e9431fc5c47
hdluong <hdluong1209@gmail.com>
2021-12-02 14:30:32 +0700

    First commit

---
+++ datasets/datasetloader.py
@@ -0,0 +1,25 @@
+# import necessary packages
+#
+#
+
+"""
+Load and preprocess the dataset from disk
+--
+--
+"""
+class DatasetLoader:
+    """
+    arguments:
+    preprocessors -- a list contain the preprocess methods
+    """
+    def __init__(self, preprocessors = None):
+        pass
+
+    """
+    Load dataset from disk
+    arguments:
+    imagePaths -- a list of file paths to images in the datasets on disk
+    flagInfo -- a flag to specify to print updates to a console or not
+    """
+    def load(self, imagePaths, flagInfo = -1):
+        pass
---
+++ preprocessing/simplepreprocessor.py
@@ -0,0 +1,26 @@
+# import necessary packages
+# 
+# 
+
+"""
+Building an simple image preprocessor
+ex: resize the image to a fixed size
+--
+"""
+class SimplePreprocessor:
+    """
+    arguments:
+    width -- width of the image
+    height -- height of the image
+    --
+    --
+    """
+    def __init__(self, width, height):
+        pass
+
+    """
+    arguments:
+    image -- the source image, numpy array
+    """
+    def preprocess(self, image):
+        pass
