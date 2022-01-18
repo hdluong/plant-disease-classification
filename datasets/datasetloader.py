ed1c9909cc188b4789163ee53af13a6f0ac03f23
hdluong <hdluong1209@gmail.com>
2022-01-18 11:21:26 +0700

    add Dataset DataLoader

--- datasets/datasetloader.py
+++ datasets/datasetloader.py
@@ -1,58 +1,33 @@
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from utils.onehot_encoder import OneHotEncoderDecoder
 import cv2
 
-"""
-Load and preprocess the dataset from disk
---
---
-"""
-class DatasetLoader:
-    """
-    arguments:
-    preprocessors -- a list contain the preprocess methods
-    """
-    def __init__(self, preprocessors = None):
-        pass
-
-    """
-    Load dataset from disk
-    arguments:
-    imagePaths -- a list of file paths to images in the datasets on disk
-    flagInfo -- a flag to specify to print updates to a console or not
-    """
-    def load(self, imagePaths, flagInfo = -1):
-        pass
-
 class Dataset:
-    def __init__(self, data, label, w, h):
+    def __init__(self, data, label, preprocessors=None):
         # the paths of images
         self.data = np.array(data)
         # the paths of segmentation images
 
         # binary encode
-        label_encoder = LabelEncoder()
-        integer_encoded = label_encoder.fit_transform(label)
+        # onehot_encoder = OneHotEncoderDecoder(label)
 
-
-        onehot_encoder = OneHotEncoder(sparse=False)
-        integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-        onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-
-
-        self.label = onehot_encoded
-        self.w = w
-        self.h = h
+        self.label = label
+        self.preprocessors = preprocessors
+        if self.preprocessors is None:
+            self.preprocessors = []
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, i):
-        print("Build model")
         # read data
         image = cv2.imread(self.data[i])
-        image = cv2.resize(image, (self.w, self.h))
+        #image = cv2.resize(image, (self.w, self.h))
+        if self.preprocessors is not None:
+            for p in self.preprocessors:
+                image = p.preprocess(image)
+        
         label = self.label[i]
         return image, label
 
