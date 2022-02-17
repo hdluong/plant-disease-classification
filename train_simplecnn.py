f51337f37d9c609d808e44e4815d70452bb18aad
Linh Trinh <linhtrinh2297@gmail.com>
2022-02-17 10:37:10 +0700

    search hyperparams

--- train_simplecnn.py
+++ train_simplecnn.py
@@ -1,3 +1,9 @@
+from tensorflow import keras
+from sklearn.metrics import classification_report
+import pickle
+import argparse
+import logging
+
 from nn.conv.simpleconvnet import SimpleNet
 from datasets.datasetloader import Dataset, Dataloader
 from preprocessing.simplepreprocessor import SimplePreprocessor
@@ -5,10 +11,7 @@ from preprocessing.hsvsegmentpreprocessor import HsvSegmentPreprocessor
 from utils.onehot_encoder import OneHotEncoderDecoder
 from datasets.dataset_utils import *
 from utils.set_logger import set_logger
-from sklearn.metrics import classification_report
-import pickle
-import argparse
-import logging
+from utils.set_params import Params
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
@@ -19,17 +22,28 @@ ap.add_argument("-l", "--label_encode", required=True,
 	help="path to output label one-hot encoder decoder")
 args = vars(ap.parse_args())
 
+# Load the parameters from json file
+json_path = os.path.join(args["model"], 'params.json')
+assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+params = Params(json_path)
+
 #train_path = "/datasets/tomatos/train/"
-train_path = args["dataset"]
-height = 256
-width = 256
-channels = 3
-batch_size = 32
-num_epochs = 20
+train_path = json_path = os.path.join(args["dataset"], 'train')
+valid_path = json_path = os.path.join(args["dataset"], 'valid')
+# height = 256
+# width = 256
+# channels = 3
+# batch_size = 32
+# num_epochs = 20
+height = params.image_size
+width = params.image_size
+channels = params.num_channels
+batch_size = params.batch_size
+num_epochs = params.num_epochs
 
 set_logger('train_simplecnn.log')
 
-X_train, y_train, X_valid, y_valid, classes = load_train_sets(train_path)
+X_train, y_train, X_valid, y_valid, classes = load_train_sets(train_path, valid_path)
 # Convert training and validation labels to one hot matrices
 label_encoding = OneHotEncoderDecoder(y_train)
 y_train = label_encoding.onehot_encoded
@@ -55,7 +69,8 @@ valid_loader = Dataloader(valid_dataset, batch_size, len(valid_dataset))
 # model
 model = SimpleNet.create(width, height, channels, classes)
 model.summary()
-model.compile(optimizer = "Adam", loss = "categorical_crossentropy",  metrics = ["accuracy"])
+# model.compile(optimizer = "Adam", loss = "categorical_crossentropy",  metrics = ["accuracy"])
+model.compile(keras.optimizers.Adam(learning_rate=params.learning_rate), loss = "categorical_crossentropy",  metrics = ["accuracy"])
 
 # print("[INFO]: training network...")
 logging.info("training network...")
