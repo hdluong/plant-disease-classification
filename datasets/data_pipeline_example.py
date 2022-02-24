e497eb9aa1629dade3c29d3884948e4ded5e5893
hoanglam20111997 <38323453+hoanglam20111997@users.noreply.github.com>
2022-02-24 09:05:07 +0700

    Add files via upload

---
+++ datasets/data_pipeline_example.py
@@ -0,0 +1,82 @@
+import numpy as np
+import os
+import tensorflow as tf
+
+data_dir=r'./dataset/train'
+batch_size = 32
+img_height = 200
+img_width = 200
+epochs = 3
+
+list_ds = tf.data.Dataset.list_files(str(data_dir + '\\*\\*'), shuffle=False)
+# get the count of image files in the train directory
+image_count=0
+
+for dir1 in os.listdir(data_dir):
+    for files in os.listdir(os.path.join(data_dir, dir1)):
+        image_count+=1
+
+list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+class_names = np.array(sorted([dir1 for dir1 in os.listdir(data_dir)]))
+
+val_size = int(image_count * 0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+
+#To process the label
+def get_label(file_path):
+  # convert the path to a list of path components separated by sep
+  parts = tf.strings.split(file_path, os.path.sep)
+  
+  # The second to last is the class-directory
+  one_hot = parts[-2] == class_names
+  # Integer encode the label
+  return tf.argmax(tf.cast(one_hot, tf.int32))
+
+# To process the image
+def decode_img(img):
+  # convert the compressed string to a 3D uint8 tensor
+  img = tf.image.decode_jpeg(img, channels=3)
+  
+  # resize the image to the desired size
+  return tf.image.resize(img, [img_height, img_width])
+
+# To create the single training of validation example with image and its corresponding label
+def process_path(file_path):
+  label = get_label(file_path)
+  # load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+def configure_for_performance(ds):
+  ds = ds.cache()
+  ds = ds.shuffle(buffer_size=1000)
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+  return ds
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+
+# tf model
+model=tf.keras.Sequential(
+        [
+            tf.keras.layers.InputLayer(input_shape=(img_height, img_width, 3)),
+            tf.keras.layers.Conv2D(filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(6)
+         ])
+
+#Compile the model
+model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+#Fitting the model
+history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
